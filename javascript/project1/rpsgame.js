5e42393b1fa9246e79c52998e716b32f937d27dc
andresg <Andres Gomez>
2022-03-07 19:11:38 -0500

    feat: add UI to the rps game

--- javascript/project1/rpsgame.js
+++ javascript/project1/rpsgame.js
@@ -1,5 +1,6 @@
 const options = ["Rock", "Paper", "Scissors"];
 
+// chose value of the computer
 function computerPlay(){
     let index = Math.floor(Math.random() * options.length);
     return options.at(index);
@@ -9,7 +10,8 @@ function firstToUpperCase(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-
+// This gives the result of a round given the selection of the computer
+//   and the selection of the player.
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
@@ -56,14 +58,17 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+// This makes the user select an option from ther terminal
 function game(){
-
     let wantToPlay = prompt("Wanna play rock, paper, scissors?");
     wantToPlay = wantToPlay.toLowerCase().replace(/\s+/g, ' ').trim();
 
-    while (wantToPlay != "yes" && wantToPlay != "no"){
+    let maxAskingTimes = 5;
+    let c = 0;
+    while (wantToPlay != "yes" && wantToPlay != "no" && c < maxAskingTimes){
         wantToPlay = prompt("Please enter a valid option. Wanna play? Yes or no?");
-        console.log(wantToPlay)
+        wantToPlay = wantToPlay.toLowerCase().replace(/\s+/g, ' ').trim();
+        c++;
     }
 
     if (wantToPlay == "yes"){
@@ -95,5 +100,51 @@ function game(){
     else{
         console.log("It is a shame, you lost a good moment");
     }
+}
+
+
+function playRoundInteractive(e){
+    console.log(this);
+    computerSelection = computerPlay();
+    playerSelection = String(e.target.classList[0]);
+    result = playRound(playerSelection, computerSelection);
+    
+    let h2 = document.querySelector(".result h2");
+    h2.textContent = result;
+
+    let userh2 = document.querySelector(".user-score");
+    let computerh2 = document.querySelector(".computer-score");
+
+    if (Number(computerh2.textContent) == 5 || Number(userh2.textContent) == 5){
+        computerh2.textContent = 0;
+        userh2.textContent = 0;
+        h2.style.color = "black";
+        h2.textContent = "New Game, please select an option";
+    }
+
+    if (result.includes("win")){
+        console.log(userh2.textContent);
+        userh2.textContent = Number(userh2.textContent) + 1;    
+    }
+    else if (result.includes("lose")){
+        computerh2.textContent = Number(computerh2.textContent) + 1;        
+    }
+
+    if (Number(computerh2.textContent) == 5 || Number(userh2.textContent) == 5){
+        if (Number(userh2.textContent) == 5){
+            h2.textContent = "You have won!! Thanks for playing";
+        }
+        else if (Number(computerh2.textContent) == 5){
+            h2.textContent = "You have lose!! Thanks for playing anyway";
+        }
+        h2.style.color = "red";
+    }
 
 }
+
+
+// Create a listener to veign playig based in user selection
+const buttons = document.querySelectorAll(".options button");
+
+//playRoundInteractive(e);
+buttons.forEach(button => button.addEventListener("click", (e) => playRoundInteractive(e)));
