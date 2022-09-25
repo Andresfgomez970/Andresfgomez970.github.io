a8736c604151f73d10a5af83e555da614211b5ea
andresg <Andres Gomez>
2022-09-24 21:32:10 -0500

    support button begin

--- code.js
+++ code.js
@@ -10,3 +10,13 @@ $('#language').on('click', function(){
    $('body').removeClass('active-modal');
    $('.language-box').removeClass('modal-show');
  })
+
+ $('#support').on('click', function(){
+  $('body').addClass('active-modal');
+  $('.support-box').addClass('modal-show');
+});
+
+$('.overlay').on('click', function(){
+  $('body').removeClass('active-modal');
+  $('.support-box').removeClass('modal-show');
+})
